4030a7c58e88210b60d98bad947fd0a93a373f33
JEroen Bosch <jeroon.bosch@gmail.com>
2015-03-12 11:44:34 +0100

    alles draak jwz

---
+++ game/Assets/scripts/IngredientsController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngredientsController : MonoBehaviour {
+	public string[] order;
+	private int count;
+	
+	// Use this for initialization
+	void Start () {
+		order = new string[8] {"ie_eggs", "ie_milk", "ie_salt", "ie_flour", "ie_bacon", "ie_water", "ie_cheese", "ie_butter"};
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		//Debug.Log("Something has entered this zone." + other.gameObject.name);
+		if (other.gameObject.name == order [count]) { 
+			Destroy (other.gameObject);
+			count++;
+		}
+	}
+}
