5b5a844182b8868f5d73f4c1a43d3dd6d3bc7f2b
JEroen Bosch <jeroon.bosch@gmail.com>
2015-03-25 13:08:29 +0100

    Introductie toegevoegd. Pas op met IngredientsController script, jouwe is waarschijnlijk degene die we willen houden?

--- game/Assets/scripts/timer.js
+++ game/Assets/scripts/timer.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 var timeRemaining : float = 120;
 var style : GUIStyle;
+var patrickFont : Font;
 
 function Start(){
 	style.normal.textColor = Color.black;
@@ -13,7 +14,8 @@ function Update () {
 function OnGUI(){
 	
     if(timeRemaining > 0){
-    	GUI.Label(new Rect(Screen.width * 0.05f , Screen.height * 0.02f , Screen.width * 0.4f , Screen.height * 0.25f), "" + Mathf.Round(timeRemaining), style);
+   		style.font = patrickFont;
+    	GUI.Label(new Rect(Screen.width * 0.05f , Screen.height * 0.02f , Screen.width * 0.4f , Screen.height * 0.25f), "Tijd: " + Mathf.Round(timeRemaining), style);
     }
     else{
         Application.LoadLevel (Application.loadedLevel);
