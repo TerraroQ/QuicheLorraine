86680b06f1a2d25033b4184f45755d1e10d49789
JEroen Bosch <jeroon.bosch@gmail.com>
2015-03-25 16:25:29 +0100

    alles of niets

--- game/Assets/scripts/DragNDrop.cs
+++ game/Assets/scripts/DragNDrop.cs
@@ -6,9 +6,11 @@ using System.Collections;
 public class DragNDrop : MonoBehaviour {
 	private Vector3 screenPoint;
 	private Vector3 offset;
+	public AudioClip[] audioClip;
 	
 	void OnMouseDown() { 
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		PlaySound(0);
 	}
 	
 	void OnMouseDrag()
@@ -16,5 +18,10 @@ public class DragNDrop : MonoBehaviour {
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 		transform.position = curPosition;
+	}
+
+	void PlaySound(int clip){
+		audio.clip = audioClip[clip];
+		audio.Play();
 	}
 }
